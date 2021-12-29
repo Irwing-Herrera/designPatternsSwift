23499edc4f91b5933ad0cbe15553e07b90cb327a
Irwing Josue Herrera Cuautle <c330593@santander.com.mx>
2021-12-29 09:55:31 -0600

    Initial Commit

---
+++ Patrones_Diseno/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Patrones_Diseno
+//
+//  Created by MacBook on 29/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
